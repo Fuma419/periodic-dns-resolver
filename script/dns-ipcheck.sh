0a9ff81f3f70f8b197a66a05c615c736ef1e62af
Zawodni Benjamin (FCA) <t8795bz@cag.cdroot.net>
2022-03-17 11:49:22 -0400

    Revert "minor code clean up"
    This reverts commit d8e1e13b51391341788108473149060a5d6ad05d.

--- script/dns-ipcheck.sh
+++ script/dns-ipcheck.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-declare -A DNS-ADDRESS
-
-NODE_ID="Mainnet Node"
-PASSIVE_MODE="true"
-SCRIPT_PATH=/opt/cardano/cnode/scripts
-DNS_PORTS=6000
+declare -A dns_address
 
 ####################################################
 # uncomment and update with DNS address(es) to allow
 ####################################################
+NODE_ID="Mainnet Node"
+PASSIVE_MODE="true"
+SCRIPT_PATH=/opt/cardano/cnode/scripts
 
-DNS-ADDRESS[your-unique-node1-location]=https://www.w3.org/
-#DNS-ADDRESS[your-unique-node2-location]=<your-unique-node2-dns-address>
-#DNS-ADDRESS[your-unique-node3-location]=<your-unique-node3-dns-address>
-#DNS-ADDRESS[your-unique-node4-location]=<your-unique-node4-dns-address>
 
+dns_address[your-node1-location]=https://www.w3.org/
+#dns_address[your-node2-location]=<your-nodes-dns-address>
+#dns_address[your-node3-location]=<your-nodes-dns-address>
+#dns_address[your-node4-location]=<your-nodes-dns-address>
+
+dns_port=6000
 
 ###############################################
 # Do not modify bellow this line
@@ -22,7 +22,7 @@ DNS-ADDRESS[your-unique-node1-location]=https://www.w3.org/
 if [ -n "$1" ] && [ "$1" == "-f" ] ; then
     PASSIVE_MODE="false"
 fi
-for i in "${DNS-ADDRESS[@]}"
+for i in "${dns_address[@]}"
 do
    : 
     if [[ $EUID -ne 0 ]]; then
@@ -46,7 +46,7 @@ do
         echo "$i was unreachable at: $old_ip. Awaiting connectivity.": $(date '+%B %d %Y %r') >> /opt/cardano/cnode/logs/dns-ipcheck.log
     # No matching firewall rule found. Lets add it now.
     elif [ -z "$old_ip" ] && [ $PASSIVE_MODE != "true" ]; then
-        /usr/sbin/ufw allow proto tcp from $new_ip to any port $DNS_PORTS comment $i
+        /usr/sbin/ufw allow proto tcp from $new_ip to any port $dns_port comment $i
         $SCRIPT_PATH/telegram-allert.sh "$NODE_ID Alert!%0A$i has no ufw rule:%0AAdding $new_ip ufw rule: %0A**Updating firewall** %0A$(date '+%B %d %Y %r')"
         echo "Could not locate $new_ip in firewall for $i: **Updating firewall**" $(date '+%B %d %Y %r') >> /opt/cardano/cnode/logs/dns-ipcheck.log
     else
@@ -56,8 +56,8 @@ do
             echo "IP address change detected on $i from $old_ip to $new_ip! Firewall NOT updated. Verify new IP and add ufw rule with  <sudo ./dns-ipcheck.sh -f>:" $(date '+%B %d %Y %r') >> /opt/cardano/cnode/scripts/dns-ipcheck.log
         else
             $SCRIPT_PATH/telegram-allert.sh "$NODE_ID Alert!%0AIP address change detected!%0A$i: %0APrevious IP:$old_ip%0ANew IP:$new_ip%0A %0A**Updating firewall now!** %0A$(date '+%B %d %Y %r')"
-            /usr/sbin/ufw delete allow proto tcp from $old_ip to any port $DNS_PORTS
-            /usr/sbin/ufw allow proto tcp from $new_ip to any port $DNS_PORTS comment $i
+            /usr/sbin/ufw delete allow proto tcp from $old_ip to any port $dns_port
+            /usr/sbin/ufw allow proto tcp from $new_ip to any port $dns_port comment $i
             echo "IP updated from $old_ip to $new_ip: "$(date '+%B %d %Y %r') >> /opt/cardano/cnode/logs/dns-ipcheck.log
         fi
     fi
